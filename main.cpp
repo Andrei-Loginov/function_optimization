18a96ca648327fa0b8da3e0d372eeb0200dc18a0
Andrei <andreyloginov1992@gmail.com>
2022-03-27 23:29:58 +0300

    Input check

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cmath>
+#include <string>
+#include <cctype>
+#include <cstdio>
 #include "function.h"
 #include "NelderMead.h"
 #include "NMSDstop.h"
@@ -35,6 +38,17 @@ double sum_of_abs(std::vector<double> x){
     return abs(x[0]) + abs(x[1]) + abs(x[2]);
 }
 
+bool is_str_double(const std::string& str){
+    if (!str.size()) return false;
+    size_t first_digit = (str[0] == '-') ? 1 : 0;
+    for (size_t i = first_digit; i < str.size(); ++i)
+        if (!isdigit(str[i])) return false;
+    return true;
+}
+
+
+
+
 int main(){
     std::cout << Rosenbrock_2d({1, 1}) << "\n";
 
@@ -57,13 +71,13 @@ int main(){
         bool flg = true;
         while (action_index != 0){
             std::cout << "0 --- exit\nany other number --- set function, method, area, etc.\n";
-            std::cin >> action_index;
+            if (!scanf("%Iu", &action_index)) throw std::invalid_argument("Wrong format!"); //action_index
             if (action_index) {
                 func_index = -1;
                 while (func_index < 1 || func_index > 4){
                     std::cout << "Set function:\n\t1 --- Rosenbrock 2d\n\t2 --- Rosenbrock 3d\n\t";
                     std::cout << "3 --- sum of abs 3d\n\t4 --- parabola 2d\n";
-                    std::cin >> func_index;
+                    if (!scanf("%Iu", &func_index)) throw std::invalid_argument("Wrong format!");
                 }
                 switch(func_index) {
                 case 1:
@@ -88,7 +102,9 @@ int main(){
                     while (flg || lower >= upper){
                         flg = false;
                         std::cout << "\tSet coordinate " << i + 1 << " bounds: ";
-                        std::cin >> lower >> upper;
+                        //std::cin >> lower >> upper;
+                        if (!scanf("%lf", &lower)) throw std::invalid_argument("Wrong format!");
+                        if (!scanf("%lf", &upper)) throw std::invalid_argument("Wrong format!");
                     }
                     limits[i] = dimension_limits(lower, upper);
                 }
@@ -99,11 +115,13 @@ int main(){
                 method_index = -1;
                 while (method_index < 1 || method_index > 2){
                     std::cout << "\n\t1 --- Stohastic\n\t2 --- Nelder-Mead\n";
-                    std::cin >> method_index;
+                    //std::cin >> method_index;
+                    if (!scanf("%Iu", &method_index)) throw std::invalid_argument("Wrong format!");
                 }
                 std::cout << "Parameters of algorithm.";
                 std::cout << "\n\t0 --- default\n\tany other number --- manual\n";
-                std::cin >> param_index;
+                //std::cin >> param_index;
+                if (!scanf("%Iu", &param_index)) throw std::invalid_argument("Wrong format!");
 
                 switch (method_index) {
                 case 1:
@@ -111,12 +129,14 @@ int main(){
                         p = -1;
                         while (p <= 0){
                             std::cout << "Set p: ";
-                            std::cin >> p;
+                            //std::cin >> p;
+                            if (!scanf("%ld", &p)) throw std::invalid_argument("Wrong format!");
                         }
                         delta = -2;
                         while (delta <= 0) {
                             std::cout << "Set delta: ";
-                            std::cin >> delta;
+                            //std::cin >> delta;
+                            if (!scanf("%lf", &delta)) throw std::invalid_argument("Wrong format!");
                         }
                     } else {
                         p = 0.5;
@@ -124,17 +144,21 @@ int main(){
                     }
                     std::cout << "Stop criterion parameters.";
                     std::cout << "\n\t0 --- default\n\tany other number --- manual\n";
-                    std::cin >> stop_index;
+                    //std::cin >> stop_index;
+                    if (!scanf("%Iu", &stop_index)) throw std::invalid_argument("Wrong format!");
+
                     if (stop_index){
                         max_iter = -1;
                         while (max_iter <= 0) {
                             std::cout << "Maximal number of iterations: ";
-                            std::cin >> max_iter;
+                            //std::cin >> max_iter;
+                            if (!scanf("%Iu", &max_iter)) throw std::invalid_argument("Wrong format!");
                         }
                         max_iter_after = 0;
                         while (max_iter_after <= 0){
                             std::cout << "Maximal number of iterations after the last improvment: ";
-                            std::cin >> max_iter_after;
+                            //std::cin >> max_iter_after;
+                            if (!scanf("%Iu", &max_iter_after)) throw std::invalid_argument("Wrong format!");
                         }
                     } else {
                         max_iter = 10000;
@@ -149,7 +173,8 @@ int main(){
                             if (first_point.size() == 0) first_point.resize(f.dim());
                             std::cout << "Enter " << f.dim() << " coordinates: \n";
                             for (size_t i = 0; i < f.dim(); ++i)
-                                std::cin >> first_point[i];
+                                //std::cin >> first_point[i];
+                                if (!scanf("%lf", &first_point[i])) throw std::invalid_argument("Wrong format!");
                         }
                     } else {
                         first_point.resize(0);
@@ -164,17 +189,20 @@ int main(){
                        alpha = -1;
                        while (alpha <= 0) {
                            std::cout << "Enter alpha > 0 : ";
-                           std::cin >> alpha;
+                           //std::cin >> alpha;
+                           if (!scanf("%lf", &alpha)) throw std::invalid_argument("Wrong format!");
                        }
                        beta = -1;
                        while (beta <= 0) {
                            std::cout << "Enter beta > 0: ";
-                           std::cin >> beta;
+                           //std::cin >> beta;
+                           if (!scanf("%lf", &beta)) throw std::invalid_argument("Wrong format!");
                        }
                        gamma = -1;
                        while (gamma <= 0){
                            std::cout << "Enter gamma > 0: ";
-                           std::cin >> gamma;
+                           //std::cin >> gamma;
+                           if (!scanf("%lf", &gamma)) throw std::invalid_argument("Wrong format!");
                        }
                     } else {
                         alpha = 1;
@@ -189,12 +217,14 @@ int main(){
                         max_iter = -1;
                         while (max_iter <= 0) {
                             std::cout << "Maximal number of iterations: ";
-                            std::cin >> max_iter;
+                            //std::cin >> max_iter;
+                            if (!scanf("%Iu", &max_iter)) throw std::invalid_argument("Wrong format!");
                         }
                         epsilon = -1;
                         while (epsilon <= 0){
                             std::cout << "Enter epsilon for Standart Deviation stop criterion: ";
-                            std::cin >> epsilon;
+                            //std::cin >> epsilon;
+                            if (!scanf("%lf", &epsilon)) throw std::invalid_argument("Wrong format!");
                         }
                     } else {
                         max_iter = 10000;
@@ -204,7 +234,9 @@ int main(){
                     init_simplex.clear();
                     std::cout << "Start simplex.";
                     std::cout << "\n\t0 --- random points\n\tany other number --- set manualy\n";
-                    std::cin >> simplex_index;
+                    //std::cin >> simplex_index;
+                    if (!scanf("%Iu", &simplex_index)) throw std::invalid_argument("Wrong format!");
+
                     if (simplex_index) {
                         std::cout << "Set " << f.dim() + 1 << " points.";
                         init_simplex.resize(f.dim() + 1);
@@ -217,7 +249,8 @@ int main(){
                                 std::cout << "Point " << i + 1;
                                 for (size_t j = 0; j < f.dim(); ++j){
                                     std::cout << "\n\tCoordinate " << j + 1 << ": ";
-                                    std::cin >> init_simplex[i][j];
+                                    //std::cin >> init_simplex[i][j];
+                                    if (!scanf("%lf", &init_simplex[i][j])) throw std::invalid_argument("Wrong format!");
                                 }
                             }
                         }
